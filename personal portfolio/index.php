99781e0b4ea0123ae29ac2b1b03e79e5eda5a263
sneax50 <salmanabdullahal0@gmail.com>
2022-03-23 14:38:55 +0600

    Add files via upload

---
+++ personal portfolio/index.php
@@ -0,0 +1,357 @@
+<?php
+
+
+
+$conn = mysqli_connect('localhost','root','','contact_db') or die('connection failed');
+
+if(isset($_POST['send'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $number = mysqli_real_escape_string($conn, $_POST['number']);
+   $msg = mysqli_real_escape_string($conn, $_POST['message']);
+
+   $select_message = mysqli_query($conn, "SELECT * FROM `contact_form` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('query failed');
+   
+   if(mysqli_num_rows($select_message) > 0){
+      $message[] = 'message sent already!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, message) VALUES('$name', '$email', '$number', '$msg')") or die('query failed');
+      $message[] = 'message sent successfully!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Complete Responsive Personal Portfolio Website Design</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
+   <!-- aos css link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/stylle.css">
+
+</head>
+<body style="background: linear-gradient(to bottom,#90EE90,#5579C6, #add8e6);">
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message" data-aos="zoom-out">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+
+?>
+
+<!-- header section starts  -->
+
+<header class="header">
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+   <a href="#home" class="logo">Portfolio</a>
+
+   <nav class="navbar">
+      <a href="#home" class="active">home</a>
+      <a href="#about">about</a>
+      <a href="#services">services</a>
+      <a href="#portfolio">portfolio</a>
+      <a href="#contact">contact</a>
+   </nav>
+
+   <div class="follow">
+      <a href="#" class="fab fa-facebook-f"></a>
+      <a href="#" class="fab fa-twitter"></a>
+      <a href="#" class="fab fa-instagram"></a>
+      <a href="#" class="fab fa-linkedin"></a>
+      <a href="#" class="fab fa-github"></a>
+   </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+   <div class="image" data-aos="fade-right">
+      <img src="images/user-img.jpeg
+      " alt="">
+   </div>
+
+   <div class="content">
+      <h3 data-aos="fade-up">hi, i am Tushar</h3>
+      <span data-aos="fade-up">web designer & developer</span>
+      <p data-aos="fade-up">Hey! This is Tushar, Expert at web development and designing.I'll make Portfolio,E-Commerce,Educational sites. Blogging Sites. I really like to figure with graphic design and are hooked in to simple, modern and luxury design using photoshop and illustrator..</p>
+      <a data-aos="fade-up" href="#about" class="btn">about me</a>
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+   <h1 class="heading" data-aos="fade-up"> <span>biography</span> </h1>
+
+   <div class="biography">
+
+      <p data-aos="fade-up">Hey! This is Tushar, Expert at web development and designing.I'll make Portfolio,E-Commerce,Educational sites. Blogging Sites. I really like to figure with graphic design and are hooked in to simple, modern and luxury design using photoshop and illustrator</p>
+
+      <div class="bio">
+         <h3 data-aos="zoom-in"> <span>name : </span> Mustofa AL Habib Tushar </h3>
+         <h3 data-aos="zoom-in"> <span>email : </span> mahtushar24@gmail.com</h3>
+         <h3 data-aos="zoom-in"> <span>address : </span> Kualalampur, Malyasia </h3>
+         <h3 data-aos="zoom-in"> <span>phone : </span> +60169401336 </h3>
+         <h3 data-aos="zoom-in"> <span>age : </span> 22 years </h3>
+         <h3 data-aos="zoom-in"> <span>experience : </span> 2+ years </h3>
+      </div>
+
+     
+
+   </div>
+   
+   <div class="skills" data-aos="fade-up">
+
+      <h1 class="heading"> <span>skills</span> </h1>
+
+      <div class="progress">
+         <div class="bar" data-aos="fade-left"> <h3><span>HTML</span> <span>95%</span></h3> </div>
+         <div class="bar" data-aos="fade-right"> <h3><span>CSS</span> <span>80%</span></h3> </div>
+         <div class="bar" data-aos="fade-left"> <h3><span>JavaScript</span> <span>65%</span></h3> </div>
+         <div class="bar" data-aos="fade-right"> <h3><span>PHP</span> <span>80%</span></h3> </div>
+         <div class="bar" data-aos="fade-left"> <h3><span>Photoshop</span> <span>85%</span></h3> </div>
+         <div class="bar" data-aos="fade-right"> <h3><span>Illustrator</span> <span>90%</span></h3> </div>
+         <div class="bar" data-aos="fade-left"> <h3><span>Wordpress</span> <span>70%</span></h3> </div>
+         <div class="bar" data-aos="fade-right"> <h3><span>Email Automation</span> <span>85%</span></h3> </div>
+      </div>
+
+   </div>
+
+   <div class="edu-exp">
+
+      <h1 class="heading" data-aos="fade-up"> <span>education & experience</span> </h1>
+
+      <div class="row">
+
+         <div class="box-container">
+
+            <h3 class="title" data-aos="fade-right">education</h3>
+
+            <div class="box" data-aos="fade-right">
+               <span>( 2015 - 2018 )</span>
+               <h3>Lincoln University College,Malaysia</h3>
+               <p>BSC in Hospitality Managment</p>
+            </div>
+
+            <div class="box" data-aos="fade-right">
+               <span>( 2011 - 2013 )</span>
+               <h3>Master Nazir Ahmed College,Bangladesh</h3>
+               <p>Higher Secondary School Certificate</p>
+              
+            </div>
+
+         </div>
+
+         <div class="box-container">
+
+            <h3 class="title" data-aos="fade-left">experience</h3>
+
+            <div class="box" data-aos="fade-left">
+               <span>( 2019 - 2020 )</span>
+               <h3>front-end developer</h3>
+               <p>HTML, CSS, BootStrap.</p>
+            </div>
+
+            <div class="box" data-aos="fade-left">
+               <span>( 2020 - 2021 )</span>
+               <h3>back-end developer</h3>
+               <p>HTML, CSS, BootStrap, JavaScript, PHP, Laravel, Wordpress, .</p>
+            </div>
+
+            
+
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+   <h1 class="heading" data-aos="fade-up"> <span>services</span> </h1>
+
+   <div class="box-container">
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-code"></i>
+         <h3>web development</h3>
+         <p>Portfolio, E-Commerce, Educational sites, Blogging sites.</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-paint-brush"></i>
+         <h3>graphic design</h3>
+         <p>Logo, Business card, Flyr, Poster, <br>Banner.</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fab fa-bootstrap"></i>
+         <h3>bootstrap</h3>
+         <p>Portfolio,E-Commerce,<br> Educational sites</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-chart-line"></i>
+         <h3>seo marketing</h3>
+         <p>Website optimization, Website Ranking, Website Userfriendly</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-bullhorn"></i>
+         <h3>digital marketing</h3>
+         <p>Facebook Marketing, Instagram Marketing, Email Marketing, Youtube Marketing</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fab fa-wordpress"></i>
+         <h3>wordpress</h3>
+         <p>Portfolio, E-Commerce, Educational sites, Blogging sites.</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- portfolio section starts  -->
+
+<section class="portfolio" id="portfolio">
+
+   <h1 class="heading" data-aos="fade-up"> <span>Meet The Team</span> </h1>
+
+   <div class="box-container">
+
+      <div class="box" data-aos="zoom-in">
+         <img src="images/user-img.jpeg" alt="">
+         <h3>Mustofa Al Habib Tushar</h3>
+         
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <img src="images/img1.jpeg" alt="">
+         <h3>Tufayel Ahmed Neil</h3>
+         
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <img src="images/img2.jpg" alt="">
+         <h3>Abdullah Al salman  </h3>
+         
+      </div>
+
+      
+
+   </div>
+
+</section>
+
+<!-- portfolio section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+   <h1 class="heading" data-aos="fade-up"> <span>contact me</span> </h1>
+
+   <form action="" method="post">
+      <div class="flex">
+         <input data-aos="fade-right" type="text" name="name" placeholder="enter your name" class="box" required>
+         <input data-aos="fade-left" type="email" name="email" placeholder="enter your email" class="box" required>
+      </div>
+      <input data-aos="fade-up" type="number" min="0" name="number" placeholder="enter your number" class="box" required>
+      <textarea data-aos="fade-up" name="message" class="box" required placeholder="enter your message" cols="30" rows="10"></textarea>
+      <input type="submit" data-aos="zoom-in" value="send message" name="send" class="btn">
+   </form>
+
+   <div class="box-container">
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-phone"></i>
+         <h3>phone</h3>
+         <p>+60169401336</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-envelope"></i>
+         <h3>email</h3>
+         <p>mahtushar24@gmail.com</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-map-marker-alt"></i>
+         <h3>address</h3>
+         <p>Kualalampur, Malaysia</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- contact section ends -->
+
+<div class="credit"> &copy; copyright @ <?php echo date('Y'); ?> by <span>SneaX-50</span> </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<!-- aos js link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+<script>
+
+   AOS.init({
+      duration:800,
+      delay:300
+   });
+
+</script>
+   
+</body>
+</html>
